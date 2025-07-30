4ea042422daea463040cb7192d335bb0b6ae4371
Juliana Neves <nevesajuliana@gmail.com>
2025-07-30 17:23:55 +0100

    update: weather icon updates with city search

--- src/index.js
+++ src/index.js
@@ -8,9 +8,13 @@ function refreshWeather(response) {
   let humidityEl = document.querySelector(".humidity");
   let feelsLikeEl = document.querySelector(".feels-like");
   let windEl = document.querySelector(".wind");
-  let temperatureInfo = document.querySelector(".temperature-info");
+  let temperatureInfo = document.querySelector("#temperature-info");
+  let timeEl = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconEl = document.querySelector("#icon-current-weather");
 
   cityEl.innerHTML = response.data.city;
+  timeEl.innerHTML = `${formatDate(date)}`;
   temperatureEl.innerHTML = `${Math.round(response.data.temperature.current)}°`;
   humidityEl.innerHTML = `${Math.round(response.data.temperature.humidity)}%`;
   feelsLikeEl.innerHTML = `${Math.round(
@@ -18,6 +22,24 @@ function refreshWeather(response) {
   )}°C`;
   windEl.innerHTML = `${Math.round(response.data.wind.speed)}km/h`;
   temperatureInfo.innerHTML = `- ${response.data.condition.description}`;
+  iconEl.innerHTML = `<img src="${response.data.condition.icon_url}" class="temperature-icon">`;
+}
+
+function formatDate(date) {
+  let day = date.getDay();
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  return `${days[day]}, ${hour}:${minutes}`;
 }
 
 function searchCity(city) {
